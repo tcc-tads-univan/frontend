f62fe1e8d82af9ffd1d76477e1a327f54adac29a
Gabril <gjperes@ufpr.br>
2023-06-18 22:00:57 -0300

    add rota aluno solicitar carona

--- src/app/aluno/aluno.routes.ts
+++ src/app/aluno/aluno.routes.ts
@@ -2,6 +2,10 @@ import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'solicitar-carona',
+    loadComponent: () => import('./solicitar-carona/solicitar-carona.page').then( m => m.SolicitarCaronaPage)
+  },
+  {
     path: 'validar-carona',
     loadComponent: () =>
       import('./validar-carona/validar-carona.page').then((m) => m.ValidarCaronaPage),
