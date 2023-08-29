6e08d3cd30c29d8d770d92ef571267198864fadb
Gabril <gjperes@ufpr.br>
2023-08-28 23:52:33 -0300

    hu08 - visual da gerenciar transporte do motorista ok

--- src/app/driver/home/home.page.ts
+++ src/app/driver/home/home.page.ts
@@ -1,22 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
 import {RouterLink} from "@angular/router";
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink, ScrollingModule],
 })
 export class HomePage implements OnInit {
-  driver = {name: 'José'}
+  selectedCampus!: { campusName: string, completeLineAddress: string };
+  _campiList = Array.from({length: 100000}).map((_, i) => _ = {
+    campusName: "campus " + i,
+    completeLineAddress: "rua pinto " + i
+  });
+  filteredCampiList = [...this._campiList];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  trackByItem(idx: number, item: any) {
+    return item.campusName;
+  }
+
+  handleInput(event: any) {
+    if (this.filteredCampiList) {
+      const query = event.target.value.toLowerCase();
+      this.filteredCampiList = this._campiList.filter((c) => c.campusName!.toLowerCase().indexOf(query) > -1);
+    }
+  }
 }
