f2659a8c4d6ef82b0e3ad314d33e2ee5368ea361
izael.cruz <izaeljunior@ufpr.br>
2023-10-13 14:32:48 -0300

    [HU03] - concluindo integração de manter van

--- src/app/driver/register-edit-vehicle/register-edit-vehicle.page.ts
+++ src/app/driver/register-edit-vehicle/register-edit-vehicle.page.ts
@@ -1,39 +1,123 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import {RouterLink} from "@angular/router";
+import {IonicModule, ToastController} from '@ionic/angular';
+import {Router, RouterLink} from "@angular/router";
+import {DriverRegistration} from "../../shared/models/driver/driver-registration";
+import {VehicleRegistration} from "../../shared/models/vehicle/vehicle-registration";
+import {DriverService} from "../../services/driver.service";
+import {LoginResponse} from "../../shared/models/user/login-response.model";
+import {LocalStorageService} from "../../services/local-storage.service";
 
 @Component({
   selector: 'app-register-edit-vehicle',
   templateUrl: './register-edit-vehicle.page.html',
   styleUrls: ['./register-edit-vehicle.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink, ReactiveFormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink, ReactiveFormsModule],
+  providers: [DriverService]
 })
 export class RegisterEditVehiclePage implements OnInit {
-  driver = {name: 'Adevaldo'}
+
+  driver = {name: this.localStorageService.loggedUser?.name}
+  isRegistered: boolean = false;
+  vehicleId: number | undefined;
+
   currentYear = new Date().getFullYear();
   vehicleForm = this.fb.group({
-    licensePlate: ['', [Validators.required]],
+    plate: ['', [Validators.required]],
     model: ['', [Validators.minLength(3), Validators.required]],
     fabricationYear: ['', [Validators.required, Validators.min(1980), Validators.max(this.currentYear)]],
-    seatsNumber: ['', [Validators.required, Validators.min(0), Validators.max(20)]],
+    seats: ['', [Validators.required, Validators.min(0), Validators.max(20)]],
   });
 
-  constructor(private fb: FormBuilder) { }
+  private loggedUser!: LoginResponse | null;
+
+  constructor(private fb: FormBuilder, private driverService: DriverService, private toastController: ToastController, private router: Router, private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
-  }
+    this.loggedUser = this.localStorageService.loggedUser;
+    if (!this.loggedUser) {
+      throw new Error("User is not logged in");
+    }
+    if (this.router.url === '/motorista/van') {
+      this.isRegistered = true;
 
-  handleSubmit() {
-    if (this.vehicleForm.valid) {
-      console.log(this.vehicleForm.value);
+      this.licensePlate?.disable({onlySelf: true});
+      this.model?.disable({onlySelf: true});
+      this.fabricationYear?.disable({onlySelf: true});
+      this.seatsNumber?.disable({onlySelf: true});
+
+      this.driverService.findDriverById(this.loggedUser.userId).subscribe(propertyValue => {
+        this.vehicleId =  propertyValue.vehicleId;
+        this.getVehicleInfos(this.vehicleId);
+      });
+
     }
   }
 
+
+  handleSubmit() {
+    if (this.vehicleForm.valid) {
+      const vehicle: VehicleRegistration = {
+        plate: this.licensePlate?.value ?? '',
+        model: this.model?.value ?? '',
+        fabricationYear: parseInt(this.fabricationYear?.value ?? '', 10),
+        seats: parseInt( this.seatsNumber?.value ?? '', 10),
+      }
+
+      // @ts-ignore
+      this.registerVehicle(vehicle, this.loggedUser.userId )
+      console.log(vehicle);
+    }
+  }
+
+  registerVehicle(vehicle: VehicleRegistration, driverId: number) {
+    // @ts-ignore
+    this.driverService.createVehicle(vehicle, driverId).subscribe({
+      next: _ => {
+        this.toastController.create({
+          message: 'Cadastro concluído!',
+          duration: 1000,
+          position: 'top',
+          color: 'success',
+          icon: 'checkmark-outline'
+        }).then(toast => toast.present());
+
+        this.router.navigate(['/']);
+      },
+      error: err => {
+        this.toastController.create({
+          message: 'Erro ao concluir o cadastro da Van',
+          duration: 1500,
+          position: 'top',
+          color: 'danger',
+          icon: 'bug-outline'
+        }).then(toast => toast.present());
+
+        console.error(`[${err.status}] ${err.message}`);
+      }
+    });
+  }
+
+  getVehicleInfos(vehicleId: number) {
+    // @ts-ignore
+    this.driverService.findVehicleById(this.loggedUser.userId, vehicleId).subscribe({
+      next: data => {
+        this.vehicleForm.setValue({
+          plate: data.plate,
+          model: data.model,
+          fabricationYear:  data.fabricationYear.toString(),
+          seats: data.seats.toString()
+        });
+      },
+      error: err => {
+        console.error("Problem trying to retireve Vehicle info");
+      }
+    });
+  }
   public get licensePlate() {
-    return this.vehicleForm.get('licensePlate');
+    return this.vehicleForm.get('plate');
   }
 
   public get model() {
@@ -45,6 +129,6 @@ export class RegisterEditVehiclePage implements OnInit {
   }
 
   public get seatsNumber() {
-    return this.vehicleForm.get('seatsNumber');
+    return this.vehicleForm.get('seats');
   }
 }
