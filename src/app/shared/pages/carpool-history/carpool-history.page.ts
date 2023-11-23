c78a3c5e5afe918d50df2f21332356d950da7ef7
izael.cruz <izaeljunior@ufpr.br>
2023-11-22 23:16:06 -0300

    hot fix: fixed history showing student name for student

--- src/app/shared/pages/carpool-history/carpool-history.page.ts
+++ src/app/shared/pages/carpool-history/carpool-history.page.ts
@@ -12,44 +12,46 @@ import {DateFormatPipe} from "../../pipes/date-format.pipe";
 import {CurrencyFormatPipe} from "../../pipes/currency-format.pipe";
 
 @Component({
-  selector: 'app-carpool-history',
-  templateUrl: './carpool-history.page.html',
-  styleUrls: ['./carpool-history.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, EmptyHistoryCardComponent, DateFormatPipe, CurrencyPipe, CurrencyFormatPipe],
-  providers: [AuthenticationService, HistoryService, ToastService]
+    selector: 'app-carpool-history',
+    templateUrl: './carpool-history.page.html',
+    styleUrls: ['./carpool-history.page.scss'],
+    standalone: true,
+    imports: [IonicModule, CommonModule, FormsModule, EmptyHistoryCardComponent, DateFormatPipe, CurrencyPipe, CurrencyFormatPipe],
+    providers: [AuthenticationService, HistoryService, ToastService]
 })
 export class CarpoolHistoryPage implements OnInit {
-  isEmpty = true;
-  tripsHistory: CarpoolHistory[] = [];
-  findCarpoolRoute!: string[];
+    isEmpty = true;
+    tripsHistory: CarpoolHistory[] = [];
+    findCarpoolRoute!: string[];
+    userType!: UserType;
 
-  constructor(private authService: AuthenticationService,
-              private historyService: HistoryService,
-              private toastService: ToastService) {
-  }
-
-  ngOnInit() {
-  }
-
-  ionViewWillEnter() {
-    const {userId, userType} = this.authService.loggedUser!;
-
-    if (userType === UserType.DRIVER) {
-      this.findCarpoolRoute = ['/motorista/caronas/procurar'];
-    } else {
-      this.findCarpoolRoute = ['/aluno/carona'];
+    constructor(private authService: AuthenticationService,
+                private historyService: HistoryService,
+                private toastService: ToastService) {
     }
 
-    this.historyService.getCarpoolHistory(userId, userType).subscribe({
-      next: data => {
-        if (data.length > 0) {
-          this.isEmpty = false;
-        }
-        this.tripsHistory = data;
-      },
-      error: err => this.toastService.showErrorToastAndLog("Problema ao recuperar o histórico", err)
-    });
-  }
+    ngOnInit() {
+    }
 
+    ionViewWillEnter() {
+        const {userId, userType} = this.authService.loggedUser!;
+        this.userType = userType;
+        if (userType === UserType.DRIVER) {
+            this.findCarpoolRoute = ['/motorista/caronas/procurar'];
+        } else {
+            this.findCarpoolRoute = ['/aluno/carona'];
+        }
+
+        this.historyService.getCarpoolHistory(userId, userType).subscribe({
+            next: data => {
+                if (data.length > 0) {
+                    this.isEmpty = false;
+                }
+                this.tripsHistory = data;
+            },
+            error: err => this.toastService.showErrorToastAndLog("Problema ao recuperar o histórico", err)
+        });
+    }
+
+    protected readonly UserType = UserType;
 }
